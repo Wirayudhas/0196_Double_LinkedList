71cc2b8d43c4b5e3e67798ea08157cad87c87f52
Wirayudhas <arturwirayuda@gmail.com>
2024-05-04 00:59:19 +0700

    membuat tampilan apabila  tidak ada data yang telah disimpan

--- Double_LinkedList/Double_LinkedList.cpp
+++ Double_LinkedList/Double_LinkedList.cpp
@@ -93,4 +93,33 @@ void deleteNode()
 	previous = NULL;
 
 
+	while (current != NULL && current->noMhs != rollNo)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+		return;
+	}
+
+	if (current == START)
+	{
+		START = START->next;
+		if (START != NULL)
+		{
+			START->prev = NULL;
+		}
+	}
+	else
+	{
+		previous->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = previous;
+		}
+	}
+
 	
